9ba934715af0805b52e6edd4e1a46a534a5c55a5
Kimleng <kimlenghor2006@gmail.com>
2023-04-04 13:15:21 -0500

    Initial Commit

---
+++ PongPong/PongPongApp.swift
@@ -0,0 +1,17 @@
+//
+//  PongPongApp.swift
+//  PongPong
+//
+//  Created by Kimleng Hor on 4/4/23.
+//
+
+import SwiftUI
+
+@main
+struct PongPongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
