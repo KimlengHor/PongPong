8e789069f40cf0ddb65d7c4d57408db53e1c5cb3
Kimleng <kimlenghor2006@gmail.com>
2023-05-06 21:05:47 -0500

    add api to check if the book is in favorite list

--- PongPong/ViewModels/BookContentViewModel.swift
+++ PongPong/ViewModels/BookContentViewModel.swift
@@ -29,19 +29,60 @@ class BookContentViewModel: ObservableObject {
         
         do {
             try await bookManager.addBookToFavorites(bookId: bookId)
-            showFeedbackView = true
             book?.setFavorite(true)
+            showFeedbackView = true
             HepticManager.instance.notification(type: .success)
             
             DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
                 self?.showFeedbackView = false
             }
-            
         } catch {
             showingAlert = true
             errorMessage = error.localizedDescription
         }
         
         isLoading = false
+    }
+    
+    func removeBookFromFavorites(book: Book?) async {
+        
+        guard let bookId = book?.id else {
+            errorMessage = "Oops! We're sorry, but at the moment you can't remove this book to your favorites list. We're working on fixing this issue and hope to have it resolved soon. Thank you for your patience and understanding!"
+            showingAlert = true
+            return
+        }
+        
+        isLoading = true
+        
+        do {
+            try await bookManager.removeBookFromFavorites(bookId: bookId)
+            
+            book?.setFavorite(false)
+            showFeedbackView = true
+            HepticManager.instance.notification(type: .success)
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
+                self?.showFeedbackView = false
+            }
+        } catch {
+            showingAlert = true
+            errorMessage = error.localizedDescription
+        }
+        
+        isLoading = false
+    }
+    
+    func checkIfBookInFavorites(book: Book?) async {
+        guard let bookId = book?.id else {
+            book?.setFavorite(false)
+            return
+        }
+        
+        do {
+            let isFavorite = try await bookManager.checkIfBookInFavorites(bookId: bookId)
+            book?.setFavorite(isFavorite)
+        } catch {
+            book?.setFavorite(false)
+        }
     }
 }
